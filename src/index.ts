ac4476e3130a4334a93c17517c3da71a3f9b3b19
vighnesh153 <vighnesh.raut13@gmail.com>
2019-12-26 20:38:28 +0530

    add tests for randomRange and add the choice feature

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import * as random from "./methods/random";
 import * as randomInt from "./methods/randomInt";
 import * as randomRange from "./methods/randomRange";
+import * as choice from "./methods/choice";
 
 export default class Random {
 
     static random = random.default;
     static randomInt = randomInt.default;
     static randomRange = randomRange.default;
+    static choice = choice.default;
 }
--- tests/Random.randomRange.test.ts
+++ tests/Random.randomRange.test.ts
@@ -1,17 +1,77 @@
 import Random from "../src";
 
 
-test('check for the randomRange method', () => {
+test('check for when no arguments are passed', () => {
 
-    const check = (start: number = 0, end: number = 1) => {
-        const randomNumber = Random.randomRange(start, end);
+    const check = () => {
+        const randomNumber = Random.randomRange();
 
-        expect(randomNumber).toBeLessThan(end);
-        expect(randomNumber).toBeGreaterThanOrEqual(start);
+        expect(randomNumber).toBeLessThan(1);
+        expect(randomNumber).toBeGreaterThanOrEqual(0);
     };
 
     check();
-    check(3, 6);
-    check(.32432, .3243243);
-    check(.227834682734, .324378643823243)
+    check();
+    check();
+    check();
+    check();
+});
+
+test('passing just 1 argument should throw', () => {
+
+    expect(() => {
+        const randomNumber = Random.randomRange(2);
+    }).toThrow();
+});
+
+test('passing invalid argument types should throw', () => {
+
+    expect(() => {
+        const randomNumber = Random.randomRange(undefined);
+    }).toThrow();
+
+    expect(() => {
+        const randomNumber = Random.randomRange(
+            undefined, undefined
+        );
+    }).toThrow();
+
+    expect(() => {
+        const randomNumber = Random.randomRange(
+            32, undefined
+        );
+    }).toThrow();
+});
+
+test('check for when start and stop are same', () => {
+
+    const check = (start: number): void => {
+        const randomNumber = Random.randomRange(start, start);
+
+        expect(randomNumber).toBe(start);
+    };
+
+    check(4);
+    check(243);
+    check(.313);
+    check(424324);
+    check(4324.5434);
+    check(75643.242);
+});
+
+test('check for the reverse arguments', () => {
+
+    const check = (start: number, end: number) => {
+        const randomNumber = Random.randomRange(start, end);
+
+        expect(randomNumber).toBeLessThan(start);
+        expect(randomNumber).toBeGreaterThanOrEqual(end);
+    };
+
+    check(434, 43);
+    check(12, 11);
+    check(-32, -45);
+    check(0, -1);
+    check(1, 0);
+    check(1, -1);
 });
