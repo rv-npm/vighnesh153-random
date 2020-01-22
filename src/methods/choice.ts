e2904e03b9291de167eec6be217db41d7ce96038
Vighnesh Raut <vighnesh.raut13@gmail.com>
2020-01-22 06:10:11 +0530

    Develop (#5)
    * fix typo in the README
    * add support for strings in random.choice
    * 1.1.0
    
    fixes: https://github.com/rv-npm/vighnesh153-random/issues/4

--- src/methods/choice.ts
+++ src/methods/choice.ts
@@ -2,17 +2,13 @@ import randomInt from "./randomInt";
 import ThrowError from "../helpers/ThrowError";
 
 
-const choice = function(sequence?: any[], ...ignoredParams: any[]) {
+const choice = function(sequence?: any, ...ignoredParams: any[]) {
 
     if (arguments.length === 0) {
         ThrowError.argumentNotSpecified('sequence');
     }
 
-    if (Array.isArray(sequence) === false) {
-        ThrowError.invalidArgumentType('sequence');
-    }
-
-    const iterable = sequence as [];
+    const iterable = Array.from(sequence);
 
     if (iterable.length === 0) {
         return undefined;
